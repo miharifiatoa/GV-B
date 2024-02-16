1421f01eed67ad86da53233594a91632813ebeab
Mihaja <mihajaodilon@gmail.com>
2024-02-16 05:54:53 +0300

    ...

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module Sales.management {
-    requires jakarta.persistence;
-    requires java.naming;
-    requires org.hibernate.orm.core;
-    opens org.sales_management.entity;
-    exports org.sales_management.entity;
-    exports org.sales_management.service;
-    exports org.sales_management.session;
-}
